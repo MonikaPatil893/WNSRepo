225135ac47322ef6380d89b275e2abaac68968c7
monikabpatil893 <95695264+monikabpatil893@users.noreply.github.com>
2021-12-08 13:05:09 +0530

    Updated Comments

--- src/test/java/TestCases/CustomUtility.java
+++ src/test/java/TestCases/CustomUtility.java
@@ -5,8 +5,11 @@ import org.openqa.selenium.WebElement;
 
 public class CustomUtility extends BaseClass {
 	
+	//Javascript utility to execute element click script.
+	
 	public void jsClick(WebElement element)
 	{
+		
 		JavascriptExecutor executor =(JavascriptExecutor)getDriver();
 		executor.executeScript("arguments[0].click();", element);
 	}
